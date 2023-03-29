7296602119eb65a1abcf2650aac4398497e01758
Carysa Peeters <carysa.peeters@student.ehb.be>
2023-03-29 10:03:32 +0200

    classes figure en rectangle gemaakt

--- js/index.js
+++ js/index.js
@@ -1,22 +1,16 @@
 "use strict";
-//import Figure from './Figure.js';
+import Figure from "./Figure.js";
 //import Circle from './Circle.js';
-//import Rectangle from './Rectangle.js';
+import Rectangle from "./Rectangle.js";
 //import Square from './Square.js';
 
 const form = {
-    changeSelect(e) {
-
-    },
-    init() {
-        console.log('Form init!');
-    },
-    inputElementsWithLabel(label, id) {
-
-    },
-    submitForm(e) {
-
-    }
+	changeSelect(e) {},
+	init() {
+		console.log("Form init!");
+	},
+	inputElementsWithLabel(label, id) {},
+	submitForm(e) {},
 };
 
 form.init();
